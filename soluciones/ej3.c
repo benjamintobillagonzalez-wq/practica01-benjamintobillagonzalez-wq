39b615e19a7cbe67a1c3a614b3e2ce6b449af7cd
BenjaminTobilla <benjamintobillagonzalez@gmail.com>
2026-03-31 08:07:50 -0600

    ej3
    Added arrays a, b, and c with pointer address printing.

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,26 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 3
+
+int main() {
+
+    int a[3];
+    int b[3];
+    int c[3];
+
+    for(int i = 0; i < 3; i++){
+        printf("a[%d] = %p\n", i, &a[i]);
+    }
+
+    printf("\n");
+
+    for(int i = 0; i < 3; i++){
+        printf("b[%d] = %p\n", i, &b[i]);
+    }
+
+    printf("\n");
+
+    for(int i = 0; i < 3; i++){
+        printf("c[%d] = %p\n", i, &c[i]);
+    }
+
     return 0;
 }
