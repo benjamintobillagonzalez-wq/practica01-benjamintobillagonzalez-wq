52e3d44360d6d461b39032054e3e5e1aa1908599
BenjaminTobilla <benjamintobillagonzalez@gmail.com>
2026-03-31 08:08:52 -0600

    ej4

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -1,6 +1,14 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 4
+
+int main() {
+
+    int a[6];
+
+    printf("sizeof(int) = %lu\n", sizeof(int));
+
+    printf("&a[0] = %p\n", &a[0]);
+    printf("&a[3] = %p\n", &a[3]);
+    printf("&a[5] = %p\n", &a[5]);
+
     return 0;
 }
