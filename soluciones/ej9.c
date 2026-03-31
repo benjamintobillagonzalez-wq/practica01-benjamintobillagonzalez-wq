c9c84970122147535321abfab51d9fe4f43202ee
BenjaminTobilla <benjamintobillagonzalez@gmail.com>
2026-03-31 08:10:10 -0600

    ej9
    Added array boundary access demonstration in ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,14 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 9
+
+int main() {
+
+    int a[5];
+
+    printf("&a[-1] = %p\n", &a[-1]);
+    printf("&a[5]  = %p\n", &a[5]);
+
     return 0;
 }
+//Compila porque C solo calcula direcciones, no valida límites.
+//Es peligroso porque accede a memoria que no pertenece al arreglo, lo que puede alterar otras variables o causar errores impredecibles.
+//El acceso fuera de rango es responsabilidad del programador.
