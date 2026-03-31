8aa9fae2a70f29d91ba752ef5c875e0079881fc4
BenjaminTobilla <benjamintobillagonzalez@gmail.com>
2026-03-31 08:13:04 -0600

    ej2
    Added pointer address comparisons for array elements.

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,15 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 2
+
+int main() {
+
+    int a[6];
+
+    printf("&a[4] = %p\n", &a[4]);
+    printf("&a[5] = %p\n", &a[5]);
+
+    if(&a[5] > &a[4]){
+        printf("a[5] está en una direccion mayor\n");
+    }
+
     return 0;
 }
