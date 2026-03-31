feda87bb3ba2af950be342905b58ce8dd5e82f6d
BenjaminTobilla <benjamintobillagonzalez@gmail.com>
2026-03-31 08:09:37 -0600

    ej6

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,14 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 6
+
+int main() {
+
+    int m[2][4];
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            printf("m[%d][%d] = %p\n", i, j, &m[i][j]);
+        }
+    }
+
     return 0;
 }
